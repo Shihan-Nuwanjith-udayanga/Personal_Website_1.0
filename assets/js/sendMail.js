465e5049bb45cccfb41a74782deafb2395ce6183
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-07-15 11:52:03 +0530

    create sendMail.js file

---
+++ assets/js/sendMail.js
@@ -0,0 +1,17 @@
+function sendEmail() {
+    Email.send({
+        SecureToken: "5c85e59c-c2b6-4a27-a23c-305c724143a1",
+        // Host : "smtp.elasticemail.com",
+        // Username : "shihannuwanjith@gmail.com",
+        // Password : "2450E6BD0C49A4C17189BBCE3D874CE7127C",
+        To : 's92067840@ousl.lk',
+        From : document.getElementById("mail").value,
+        Subject : "New Contact Form Enquiry",
+        Body : "Name: " + document.getElementById("name").value
+            + "<br> Email: " +  document.getElementById("mail").value
+            + "<br> Phone No: " +  document.getElementById("tel").value
+            + "<br> Message: " +  document.getElementById("message").value
+    }).then(
+        message => alert("Message Sent Successfully")
+    );
+}
